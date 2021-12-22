60c624eb7ea9b158ec3ea75efd5a33720f61a1f8
Hao Wang <hao.wang@shopee.com>
2021-12-22 21:22:29 +0800

    feat: add apis

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import redis from "../../lib/redis";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const name = await redis.get('name')
+  res.status(200).json({ name })
 }
