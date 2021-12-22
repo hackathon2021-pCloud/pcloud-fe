cb314f03d4dcb2db773cc7575b66d2d347116595
Hao Wang <hao.wang@shopee.com>
2021-12-22 21:30:10 +0800

    !fix: grammar

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import redis from "../../lib/redis";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const name = await redis.get('name')
   res.status(200).json({ name })
 }
