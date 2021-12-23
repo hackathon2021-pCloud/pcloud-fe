51feb9c3553cea690bbefcf7fec7cfc9f260f4be
Wang Hao <wanghaoipv6@gmail.com>
2021-12-23 14:15:23 +0800

    !fix: image

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,9 @@
 import * as styles from './Avatar.module.css'
-import Image from "next/image";
 
 export default function Avatar({user}) {
 	return (
     <div className={styles.wrapper}>
-      <Image src={user.picture} className={styles.image} />
+      <img src={user.picture} className={styles.image} alt={user.name} />
     </div>
   );
 }
--- components/Logo.js
+++ components/Logo.js
@@ -1,12 +1,16 @@
 import cx from "classnames";
-import noBgLogo from "../public/LOGO-v1.png";
-import bgLogo from "../public/LOGO-v1-bg.png";
+import noBgLogo from "../public/logo-v1.svg";
+import bgLogo from "../public/logo-v1-bg.svg";
 import * as styles from "./Logo.module.css";
 import Image from "next/image";
+
+const IMAGE_SIZE = {
+  normal: {width: 50, height: 50 }
+}
 
 export default function Logo({ size = "normal", type = "no-background" }) {
   const image = type === "no-background" ? noBgLogo : bgLogo;
   return (
-    <Image src={image.src} className={cx(styles.logo, styles[`size${size}`])} />
+    <Image src={image.src} className={styles.logo} {...IMAGE_SIZE[size]} alt="pCloud" />
   );
 }
